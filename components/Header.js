71fb7d713634116f4bb7845978788fec78e49cee
Lucy Toman <lucy@toman.me.uk>
2022-08-29 12:23:49 +0200

    adds outline text to home page

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,15 @@ import Head from "next/head"
 
 export default function Header() {
     return (
-        <Head>
-            <title>Lucyville</title>
-            <meta name="description" content="review site" />
-            <link rel="icon" href="/house.ico" />
-        </Head>
+        <>
+            <Head>
+                <title>Lucyville</title>
+                <meta name="description" content="review site" />
+                <link rel="icon" href="/house.ico" />
+            </Head>
+            <header className="w-full border-b-2 border-black text-center font-black text-4xl py-10">
+                Welcome to Lucyville
+            </header>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,41 @@
 export default function Home() {
-    return <div>index.js holding text</div>
+    return (
+        <>
+            <div className="text-center ">
+                <h1 className="mt-10 font-extrabold text-2xl">
+                    The best in town
+                </h1>
+
+                <div className="grid md:grid-cols-3">
+                    <div>
+                        <h2 className="mt-10 font-bold">Restaurants</h2>
+
+                        <ol className="mt-4 list-inside list-decimal">
+                            <li>Restaurant 1</li>
+                            <li>Restaurant 2</li>
+                            <li>Restaurant 3</li>
+                        </ol>
+                    </div>
+                    <div>
+                        <h2 className="mt-10 font-bold">Hotels</h2>
+
+                        <ol className="mt-4 list-inside list-decimal">
+                            <li>Hotel 1</li>
+                            <li>Hotel 2</li>
+                            <li>Hotel 3</li>
+                        </ol>
+                    </div>
+                    <div>
+                        <h2 className="mt-10 font-bold">Things to do</h2>
+
+                        <ol className="mt-4 list-inside list-decimal">
+                            <li>Thing 1</li>
+                            <li>Thing 2</li>
+                            <li>Thing 3</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
