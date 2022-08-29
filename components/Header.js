f2d44d0fe07b8303a22f161cae78c65572aaca96
Lucy Toman <lucy@toman.me.uk>
2022-08-29 11:52:44 +0200

    initial setup

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Head from "next/head"
+
+export default function Header() {
+    return (
+        <Head>
+            <title>Lucyville</title>
+            <meta name="description" content="review site" />
+            <link rel="icon" href="/house.ico" />
+        </Head>
+    )
+}
